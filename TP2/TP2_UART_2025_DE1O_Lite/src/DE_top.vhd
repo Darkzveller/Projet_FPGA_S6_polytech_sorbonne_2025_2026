2b396a397bf4ac1ddacf8f1ae2907b7b140a0350
Darkzveller <elkattoufi.youssef@gmail.com>
2026-04-08 19:36:01 +0200

    MAJ de securite

--- TP2/TP2_UART_2025_DE1O_Lite/src/DE_top.vhd
+++ TP2/TP2_UART_2025_DE1O_Lite/src/DE_top.vhd
@@ -12,16 +12,16 @@ library IEEE;
     Entity DE_top is
 -- -----------------------------------------------------------------
   port (CLOCK_50 	:  IN  STD_LOGIC;
-		-- UART_TXD  :  OUT  STD_LOGIC;
+		--UART_TXD  :  OUT  STD_LOGIC;
 		--UART_RXD  :  IN STD_LOGIC;
 		GPIO_0    :  INOUT STD_LOGIC_VECTOR(3 downto 0);
 		KEY			 	:  IN  STD_LOGIC_VECTOR(2 DOWNTO 0);
 		SW 				:  IN  STD_LOGIC_VECTOR(9 DOWNTO 0);
 		LED : out std_logic
-		--HEX0 			:  OUT  STD_LOGIC_VECTOR(0 TO 6);
-		--HEX1 			:  OUT  STD_LOGIC_VECTOR(0 TO 6);
-		--HEX2 			:  OUT  STD_LOGIC_VECTOR(0 TO 6);
-		--HEX3 			:  OUT  STD_LOGIC_VECTOR(0 TO 6)     
+		HEX0 			:  OUT  STD_LOGIC_VECTOR(0 TO 6);
+		HEX1 			:  OUT  STD_LOGIC_VECTOR(0 TO 6);
+		HEX2 			:  OUT  STD_LOGIC_VECTOR(0 TO 6);
+		HEX3 			:  OUT  STD_LOGIC_VECTOR(0 TO 6)     
            );
 end entity ;
 
@@ -35,7 +35,6 @@ signal Data : STD_LOGIC_VECTOR(7 DOWNTO 0);
 signal LD : std_logic;
 signal Tick : std_logic;
 signal Tx_Busy : std_logic;
---signal led : std_logic;
 
 ---------
 begin
@@ -43,10 +42,10 @@ begin
 
 rst <= not KEY(0);
 clk <= CLOCK_50; 
---UART_TXD <= Tx;
+UART_TXD <= Tx;
 Data <= Sw(7 downto 0);
 LD <= not KEY(1);
-led <= Tx_Busy;
+LED <= Tx_Busy;
 -- sur les cartes DE, les signaux UART_TX et UART_RX sont difficilement accessible
 -- Il faut mieux utiliser les ports extensions GPIO_0 et GPIO_1
 -- par exemple GPIO_0(0) pour Tx et GPIO_0(1) pour Rx 
@@ -66,16 +65,16 @@ U1 : entity work.FDIV
         Tick_half=> open
 	);
 
-	UART : entity work.UART_TX 
-    	port map(
-    	    Clk   => clk,
-    	    Reset => rst,
-    	    LD    => LD,
-    	    Din  => Data,
-    	    Tick => Tick,
-    	    Tx_Busy  => Tx_Busy,
-    	    Tx    => Tx
-    	);
+UART : entity work.UART_TX 
+	port map(
+	    Clk   => clk,
+	    Reset => rst,
+	    LD    => LD,
+	    Din  => Data,
+	    Tick => Tick,
+	    Tx_Busy  => Tx_Busy,
+	    Tx    => Tx
+	);
 
 
 
