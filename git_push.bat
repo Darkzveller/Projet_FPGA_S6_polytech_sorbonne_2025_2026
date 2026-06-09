1a9ef786d55e09ef477f02c91a0fca63958dd9b0
Darkzveller <elkattoufi.youssef@gmail.com>
2026-06-09 09:01:52 +0200

    Decodeur fini mais non tester et proc mono cycle en cours de construction

--- git_push.bat
+++ git_push.bat
@@ -1,2 +1,2 @@
 
-git status && git add . && git commit -m "partie 3 officiellement fini avec des commentaires pour aider" && git push
+git status && git add . && git commit -m "Decodeur fini mais non tester et proc mono cycle en cours de construction" && git push
